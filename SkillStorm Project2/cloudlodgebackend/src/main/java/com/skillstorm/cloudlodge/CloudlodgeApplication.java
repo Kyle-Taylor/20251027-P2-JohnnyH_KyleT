d891eafce405a1d0f85b31a452c42e49171172e7
Kyle-Taylor <kyletaylor0116@gmail.com>
2025-11-26 14:04:30 -0600

    Initial Push

---
+++ SkillStorm Project2/cloudlodgebackend/src/main/java/com/skillstorm/cloudlodge/CloudlodgeApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.cloudlodge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudlodgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudlodgeApplication.class, args);
+	}
+
+}
